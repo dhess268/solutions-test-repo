a0af4777814925e8f79e216edb6c75052e5a63d1
Dillon Hess <83610287+dhess268@users.noreply.github.com>
2022-05-21 01:34:17 -0400

    Create sol.js

---
+++ sol.js
@@ -0,0 +1,82 @@
+const findSum = function(array) {
+  return array.reduce( ((prev, curr) => prev + curr), 0 )
+};
+
+const findFrequency = function(array) {
+    let noDupes = [...new Set(array)]
+
+    let most = noDupes.reduce((prev, curr) => {
+        return noDupes.filter(elem => elem === curr).length > prev ? curr : prev
+    }, noDupes[0])
+  
+    let least =noDupes.reduce((prev, curr) => {
+        return noDupes.filter(elem => elem === curr).length > prev ? prev : curr
+    }, noDupes[0])
+
+    return {
+        'most': most,
+        'least': least
+    }
+};
+
+const isPalindrome = function(str) {
+ return str.toLowerCase() === str.toLowerCase().split('').reverse().join('')
+};
+
+const largestPair = function(array) {
+  let productArr = []
+    for(let i = 0; i < array.length - 1; i++){
+        productArr.push(array[i] * array[i+1])
+    }
+    return Math.max(...productArr)
+};
+
+const removeParenth = function(str) {
+  let start = str.indexOf('(')
+    let end = str.indexOf(')')
+    let strArr = str.split('')
+    // console.log(start, end)
+    // console.log(strArr) 
+    strArr.splice(start, end -start + 1)
+    // console.log(strArr)
+    
+    return strArr.join('')
+};
+
+const scoreScrabble = function(str) {
+    let one = ['a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't']
+    let two = ['d', 'g']
+    let three = [ 'b', 'c', 'm', 'p']
+    let four = ['f', 'h', 'v', 'w', 'y']
+    let five = ['k']
+    let eight = [ 'j', 'x']
+    let ten = ['q', 'z']
+
+    let count = 0
+    
+    for(let letter of str.split('')){
+        if(one.includes(letter)){
+            count += 1
+        }
+        else if(two.includes(letter)){
+            count += 2
+        }
+        else if(three.includes(letter)){
+            count += 3
+        }
+        else if(four.includes(letter)){
+            count += 4
+        }
+        else if(five.includes(letter)){
+            count += 5
+        }
+        else if(eight.includes(letter)){
+            count += 8
+        }
+        else if(ten.includes(letter)){
+            count += 10
+        }
+
+    }
+    return count
+};
